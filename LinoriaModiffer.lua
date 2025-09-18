762f38072df69e07f19fe8c564092a5dff1eacc6
Device_21 <Device123198356@gmail.com>
2025-09-19 00:16:37 +0300

    LinoriaModiffer.lua

--- LinoriaModiffer.lua
+++ LinoriaModiffer.lua
@@ -8,11 +8,6 @@ local TweenService = game:GetService('TweenService');
 local RenderStepped = RunService.RenderStepped;
 local LocalPlayer = Players.LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
-local function AddCorner(obj, radius)
-    local c = Instance.new("UICorner")
-    c.CornerRadius = UDim.new(0, radius or 8)
-    c.Parent = obj
-end
 
 local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
